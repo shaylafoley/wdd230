9da8e00ab0e62f27de09c2a0580205228a6d60c3
shaylafoley <wholemotherhood@gmail.com>
2024-05-03 14:46:23 -0400

    hamburger and dark

---
+++ scripts/darkMode.js
@@ -0,0 +1,9 @@
+const myBtn = document.getElementById('darkBtn');
+
+
+myBtn.addEventListener('click', () => {
+    // Toggle dark mode class on the main content area
+    main.classList.toggle('dark');
+});
+
+
---
+++ scripts/hamburger.js
@@ -0,0 +1,8 @@
+const hamburgerElement = document.querySelector('#myButton');
+const navElement = document.querySelector('#animateMe');
+
+hamburgerElement.addEventListener('click', () => {
+    navElement.classList.toggle('open');
+    hamburgerElement.classList.toggle('open');
+})
+
