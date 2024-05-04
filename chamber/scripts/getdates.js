d15fc97363aa289a48df22d8e87105e8c261403c
shaylafoley <wholemotherhood@gmail.com>
2024-05-04 12:41:54 -0400

    chamber website

---
+++ chamber/scripts/getdates.js
@@ -0,0 +1,6 @@
+const currentYear = new Date().getFullYear();
+const lastModified = new Date(document.lastModified);
+
+document.getElementById('copyYear').innerHTML = `Copyright &copy; ${currentYear} &bull; Shayla Foley &bull; WDD 230 &bull`;
+document.getElementById('lastMod').innerHTML = `Last modified: ${lastModified}`;
+
