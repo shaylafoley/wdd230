75d58914601affcbe6df5e8c0ef3f2e5578ba3cc
shaylafoley <wholemotherhood@gmail.com>
2024-05-07 21:08:50 -0400

    team activity

---
+++ scripts/localStorage.js
@@ -0,0 +1,10 @@
+const visitsDisplay = document.querySelector(".visits");
+let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
+if (numVisits !== 0) {
+	visitsDisplay.textContent = `This is your first visit. 🥳 Welcome!`;
+} else {
+	visitsDisplay.textContent = numVisits;
+}
+numVisits++;
+localStorage.setItem("numVisits-ls", numVisits);
+
