8d0f74f1095413a98c9c682e156ffc1fed5987b0
shaylafoley <wholemotherhood@gmail.com>
2024-05-07 21:23:23 -0400

    storage fix

--- scripts/localStorage.js
+++ scripts/localStorage.js
@@ -1,10 +1,11 @@
 const visitsDisplay = document.querySelector(".visits");
 let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
-if (numVisits !== 0) {
+if (numVisits === 0) {
 	visitsDisplay.textContent = `This is your first visit. 🥳 Welcome!`;
-} else {
-	visitsDisplay.textContent = numVisits;
-}
+} 
+else {
+	visitsDisplay.textContent = `This is your visit number ${numVisits+1}.`;
+};
 numVisits++;
 localStorage.setItem("numVisits-ls", numVisits);
 
