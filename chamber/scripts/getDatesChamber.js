b3cf5a775afcad5e1d97ec09f782a4f9721ad5e9
shaylafoley <wholemotherhood@gmail.com>
2024-06-04 14:08:20 -0400

    scripts getDates

--- chamber/scripts/getDatesChamber.js
+++ chamber/scripts/getDatesChamber.js
@@ -1,15 +1,15 @@
-document.addEventListener("DOMContentLoaded", function() {
+/*document.addEventListener("DOMContentLoaded", function() {
     const currentYear = new Date().getFullYear();
     const lastModified = new Date(document.lastModified);
 
     document.getElementById('copyYear').innerHTML = `Copyright &copy; ${currentYear} &bull; Shayla Foley &bull; WDD 230 &bull`;
     document.getElementById('lastMod').innerHTML = `Last modified: ${lastModified.toLocaleDateString()} ${lastModified.toLocaleTimeString()}`;
 });
+*/
 
-
-/*const currentYear = new Date().getFullYear();
+const currentYear = new Date().getFullYear();
 const lastModified = new Date(document.lastModified);
 
 document.getElementById('copyYear').innerHTML = `Copyright &copy; ${currentYear} &bull; Shayla Foley &bull; WDD 230 &bull`;
 document.getElementById('lastMod').innerHTML = `Last modified: ${lastModified}`;
-*/
+
